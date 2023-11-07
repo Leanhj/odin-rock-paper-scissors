9bf3f718962dd7d129aedbd4082344a28f2c9c92
lhiga <lhiga@dc.uba.ar>
2023-11-06 22:44:59 -0300

    Add getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
