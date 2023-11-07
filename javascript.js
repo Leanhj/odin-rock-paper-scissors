43eb70c75640f2c71d38fd2378be87f4a7ceda7a
lhiga <lhiga@dc.uba.ar>
2023-11-06 22:55:33 -0300

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -2,12 +2,46 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toLowerCase();
+
+    if (player === computerSelection) {
+        return 0;
+    }
+
+    if (player === "rock") {
+        if (computerSelection === "paper") {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+    
+    if (player === "paper") {
+        if (computerSelection === "rock") {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    if (player === "scissors") {
+        if (computerSelection === "rock") {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
